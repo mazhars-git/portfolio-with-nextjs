47dec665d59829cc359b60fa729a6e86e4ca4903
mazhars-git <mazharulislam611@gmail.com>
2026-01-01 23:56:55 +0600

    update text

--- src/components/LoginForm/Login-form.tsx
+++ src/components/LoginForm/Login-form.tsx
@@ -21,7 +21,9 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-3xl font-bold">Welcome back</h1>
+                <h1 className="bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent capitalize text-4xl font-extrabold">
+                  Welcome back
+                </h1>
                 <p className="text-muted-foreground text-balance">
                   Login to your portfolio
                 </p>
@@ -48,13 +50,18 @@ export function LoginForm({
                 <Input id="password" type="password" required />
               </Field>
               <Field>
-                <Button type="submit">Login</Button>
+                <Button
+                  className="bg-gradient-to-r from-indigo-500 to-purple-600 text-center text-white px-4 py-2 rounded-full shadow-lg hover:from-indigo-700 hover:to-purple-900 transition"
+                  type="submit"
+                >
+                  Login
+                </Button>
               </Field>
               <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
                 Or continue with
               </FieldSeparator>
-              <Field className="grid grid-cols-3 gap-4">
-                <Button variant="outline" type="button">
+              <Field className="grid grid-cols-2 gap-4">
+                {/* <Button variant="outline" type="button">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
@@ -62,7 +69,7 @@ export function LoginForm({
                     />
                   </svg>
                   <span className="sr-only">Login with Apple</span>
-                </Button>
+                </Button> */}
                 <Button variant="outline" type="button">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
